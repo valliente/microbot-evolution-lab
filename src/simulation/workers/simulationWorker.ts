98e8e06576710479b71d3c87930a34949a366ec2
valliente <valliente@users.noreply.github.com>
2026-07-27 14:54:47 +0200

    refactor(genetics): offload DNA mutation and crossover functions to background worker

--- src/simulation/workers/simulationWorker.ts
+++ src/simulation/workers/simulationWorker.ts
@@ -19,5 +19,18 @@ self.onmessage = (event: MessageEvent) => {
       results.push({ botId: bots[i].id, closestFoodId, distance: Math.sqrt(closestDist) });
     }
     self.postMessage({ type: 'DISTANCES_CALCULATED', payload: results });
+  } else if (type === 'CROSSOVER_DNA') {
+    const { parentA, parentB, mutationRate } = payload;
+    const offspring = {
+      speed: Math.random() < 0.5 ? parentA.speed : parentB.speed,
+      visionRadius: Math.random() < 0.5 ? parentA.visionRadius : parentB.visionRadius,
+      energyEfficiency: Math.random() < 0.5 ? parentA.energyEfficiency : parentB.energyEfficiency,
+      hue: Math.random() < 0.5 ? parentA.hue : parentB.hue
+    };
+    if (Math.random() < mutationRate) {
+      offspring.speed += (Math.random() - 0.5) * 0.4;
+      offspring.visionRadius += (Math.random() - 0.5) * 15;
+    }
+    self.postMessage({ type: 'DNA_CROSSOVER_COMPLETE', payload: offspring });
   }
 };
