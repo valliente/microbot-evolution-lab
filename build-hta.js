b2081050104663d1336bafe659a4677dfa476ab5
valliente <valliente@users.noreply.github.com>
2026-07-27 15:06:18 +0200

    fix(paths): ensure static assets resolve correctly under packaged file:// protocols

--- build-hta.js
+++ build-hta.js
@@ -15,6 +15,7 @@ const htaHeader = `<!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta charset="utf-8" />
 <title>Microbot Evolution Lab - 0.1.204</title>
 <HTA:APPLICATION
   ID="MicrobotEvolutionLabApp"
