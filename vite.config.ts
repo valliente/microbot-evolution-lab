73160705c3888502d6d3b492bc3126c984538b3e
valliente <valliente@users.noreply.github.com>
2026-08-07 17:14:50 +0200

    build(assets): bundle all 3D WebGL shaders, worker scripts, and UI assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './',
-  assetsInclude: ['**/*.wasm'],
+  assetsInclude: ['**/*.wasm', '**/*.glsl', '**/*.vert', '**/*.frag'],
   worker: {
     format: 'iife'
   },
